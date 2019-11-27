459856d8e5019b1fe39518892ee313f2418ed7df
Kevin Liebholz <kevin@liebholz.eu>
2019-11-27 16:52:13 +0100

    ReviewsController and views CRUD and adding reviews to restaurants#show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,28 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create]
+
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(params_permitted)
+    @review.restaurant = @restaurant
+    # @review.restaurant_id = params[:restaurant_id].to_i
+    if @review.save
+      redirect_to restaurant_path(Restaurant.find(params[:restaurant_id]))
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_permitted
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
